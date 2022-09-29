6d0a5f7bcc7a39b8970bee62300258e95197d785
zrs <1042096282@qq.com>
2022-09-29 09:44:08 +0800

    日志系统的用户

--- utils/file.py
+++ utils/file.py
@@ -5,7 +5,8 @@ import logging
 初始化建立文件系统
 """
 def init_log(vt_symbol):
-    path = 'C:/Users/Administrator/strategies/logs/'
+    login_user = os.getlogin()
+    path = 'C:/Users/' + login_user + '/strategies/logs/'
     if not os.path.exists(path):
         os.mkdir(path)
     day = time.strftime("%Y-%m-%d", time.localtime())
